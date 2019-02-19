aad0db92641c93be37978f382659b0e86e2f83df
DaSuSu <gzqd201802@163.com>
2019-02-19 17:15:07 +0800

    轮播图和导航分类完成

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -10,26 +10,56 @@
     <swiper
       indicator-dots
       autoplay
+      circular
+      indicator-color="rgba(255,255,255,0.3)"
+      indicator-active-color="#fff"
+      class="index-slide"
     >
       <block v-for="(item,index) in imgUrl" :key="index">
         <swiper-item>
-          <image :src="item" class="slide-image" />
+          <image :src="item.image_src" class="slide-image" mode="widthFix"></image>
         </swiper-item>
       </block>
     </swiper>
-
+    <!-- 首页分类 -->
+    <div class="menu">
+      <div class="menu-item" v-for="(item,index) in menus" :key="index">
+        <img class="menu-img" :src="item.image_src"></img>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default{
   data(){
     return{
-      imgUrl:[
-        "https://img.alicdn.com/tfs/TB1N7tuGY2pK1RjSZFsXXaNlXXa-520-280.jpg",
-        "https://aecpm.alicdn.com/simba/img/TB1JNHwKFXXXXafXVXXSutbFXXX.jpg",
-        "https://img.alicdn.com/tfs/TB1nZhAG7voK1RjSZFNXXcxMVXa-520-280.jpg"
-      ]
+      // 存放轮播图数据的数组
+      imgUrl:[],
+      // 分类数据
+      menus:[]
     }
+  },
+  mounted () {
+    // 缓存 this 对象
+    let that = this;
+    // 发送请求获取轮播图数据
+    wx.request({
+      url:"https://itjustfun.cn/api/public/v1/home/swiperdata",
+      success(res){
+        // 获取返回的数据
+        let data = res.data.data;
+        // 把数据赋值给 imgUrl
+        that.imgUrl = data;
+      }
+    });
+    wx.request({
+      url:"https://www.itjustfun.cn/api/public/v1/home/catitems",
+      // 箭头函数
+      success:(res)=>{
+        console.log(res);
+        this.menus = res.data.data;
+      }
+    });
   }
 }
 </script>
@@ -51,4 +81,21 @@ export default{
   .search-icon{
     margin-right: 16rpx;
   }
+  .index-slide{
+    height: 340rpx;
+  }
+  .slide-image {
+    width: 100%;
+    height: 100%;
+  }
+  .menu{
+    display: flex;
+    justify-content: space-around;
+    padding: 24rpx;
+  }
+  .menu-img{
+    width: 128rpx;
+    height: 140rpx;
+  }
+
 </style>
